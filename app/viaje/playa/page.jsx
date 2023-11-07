c6737061f629112d2514a2a600e9094069007334
onereadytwo <estudioprogramador.jose@gmail.com>
2023-11-06 22:06:49 -0300

    creando carpeta  out

--- app/viaje/playa/page.jsx
+++ app/viaje/playa/page.jsx
@@ -1,9 +1,37 @@
+import Link from 'next/link'
 import React from 'react'
+import styles from '../../stylehome.module.css'
 
 export default function BeachPage() {
   return (
     <div>
-      <h1>playas</h1>
+        <div className={styles.backgroundImage_2}>
+                  <h1 className={`fs-5 ${styles.colorText}`} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', fontWeight:"bolder" }}>
+                    
+                  Las 7 Mejores Playas para ir 
+                    </h1>
+                  </div>
+                  <div className='container '  style={{  padding: "1px", textAlign: "justify", textJustify: "inter-word"}} >
+                       <p style={{  padding: "10px", textAlign: "justify", textJustify: "inter-word"}} >Imagínate un mundo donde la arena dorada se funde con aguas cristalinas, y la brisa salada acaricia tu piel mientras contemplas la inmensidad del océano 
+                       Playa de Bora Bora, Tahití: No hay lugar como el paraíso en la Tierra, y Bora Bora es un claro ejemplo de ello. Sus aguas turquesas y la exuberante vegetación crean un escenario de ensueño. </p>
+
+<p style={{  padding: "10px", textAlign: "justify", textJustify: "inter-word"}}>Playa de Anse Lazio, Seychelles: En esta playa, la arena fina y el agua tibia se combinan para formar un rincón mágico. Las palmeras se mecen suavemente, invitándote a relajarte.</p>
+
+<p style={{  padding: "10px", textAlign: "justify", textJustify: "inter-word"}}>Playa de Navagio, Zakynthos, Grecia: También conocida como Shipwreck Beach, esta joya escondida es famosa por su barco naufragado que yace en la arena dorada, creando un escenario único.</p>
+
+<p style={{  padding: "10px", textAlign: "justify", textJustify: "inter-word"}}>Playa de Whitehaven, Australia: La arena blanca y las aguas de múltiples tonalidades de azul hacen que Whitehaven sea una maravilla natural. Es un lugar que te dejará sin aliento.</p>
+
+<p style={{  padding: "10px", textAlign: "justify", textJustify: "inter-word"}}>Playa de Tulum, México: Rodeada de ruinas antiguas y la selva tropical, esta playa es una mezcla única de historia y belleza natural. Las aguas cristalinas son ideales para nadar.</p>
+
+<p style={{  padding: "10px", textAlign: "justify", textJustify: "inter-word"}}>Playa de Flamenco, Culebra, Puerto Rico: Este rincón caribeño es una joya escondida. La suave arena y las aguas tranquilas son un refugio perfecto para aquellos que buscan escapar del bullicio.</p>
+
+<p style={{  padding: "10px", textAlign: "justify", textJustify: "inter-word"}}>Playa de Palma, Mallorca: Y finalmente, llegamos a Mallorca, una isla mediterránea que combina la belleza de las playas con un ambiente encantador. La Playa de Palma, con su extensa franja de arena y el cálido sol mediterráneo, te invita a disfrutar de la vida a tu propio ritmo. La brisa marina y la vista de la bahía crean una sensación de tranquilidad y asombro que te envolverá.</p>
+
+<p style={{  padding: "10px", textAlign: "justify", textJustify: "inter-word"}}>Cada una de estas playas es un regalo de la naturaleza, un recordatorio de la belleza infinita de nuestro planeta. Desde la lejana Bora Bora hasta la encantadora Mallorca, estas playas te ofrecen una experiencia única, llena de emociones y momentos inolvidables. Sumérgete en sus aguas, relájate en la arena y descubre la magia que solo las mejores playas del mundo pueden ofrecer. ¡Tu alma viajará a lugares que nunca imaginaste!</p>
+
+                       
+                  </div>
+                  
     </div>
   )
 }
